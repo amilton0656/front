0aec250230bd73925dcd6cfa0902670498104cc7
amilton1313 <amilton1313@gmail.com>
2022-03-06 19:59:56 -0300

    2022 03 06

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+import IntraneLayout from './modulos/intranet/IntranetLayout'
+
+function App() {
+  return (
+    
+    <div className='app-main'>
+        <IntraneLayout />
+    </div>
+    )
+  }
+
+  export default App;
